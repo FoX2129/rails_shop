f1cbe3f9ccd2398f1d1cf29bc48515420d525bb6
FoX_2129 <fox2129@gmail.com>
2021-03-30 08:26:32 +0300

    Carts and reference to items #Issue 7258

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_063022) do
+ActiveRecord::Schema.define(version: 2021_03_26_073956) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
