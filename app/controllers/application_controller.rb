bf74e59984a1216b91d9fd2c3d9fe671bf33a248
FoX_2129 <fox2129@gmail.com>
2021-04-27 17:32:30 +0300

    Shop. #Issue 7258

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
+
   include CurrentCart
   before_action :set_cart
+
+  protected
+  
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to login_url, notice: "Пожалуйста, пройдите авторизацию"
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -53,21 +53,26 @@ class UsersController < ApplicationController
 
   # DELETE /users/1 or /users/1.json
   def destroy
-    @user.destroy
+    begin
+      @user.destroy
+      flash[:notice] = "Пользователь #{@user.name} удален"
+    rescue StandardError => e
+      flash[:notice] = e.message
+    end
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :password, :password_confirmation)
-    end
+  # Only allow a list of trusted parameters through.
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
